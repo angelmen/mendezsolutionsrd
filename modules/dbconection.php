a630b7865290f2bf71bfaca1814ea92dcf3c8e5e
Angel Mendez <admd1112@gmail.com>
2020-11-30 17:41:07 -0400

    restore getenv on dbconnection.php

--- modules/dbconection.php
+++ modules/dbconection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = getenv("mendezsolutionsrd_mysqluser");
+$password = getenv("mendezsolutionsrd_mysqlpass");
 $dbname = "proyectofinal";
 
 try {
