ba5457af885e479914d8064dda6afef9fa45e92e
Angel Mendez <admd1112@gmail.com>
2020-11-30 16:55:19 -0400

    require user credentials from env

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "proyectofinal";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-}
+    @ require_once('/modules/dbconection.php')
 ?>
 
 <!DOCTYPE html>
